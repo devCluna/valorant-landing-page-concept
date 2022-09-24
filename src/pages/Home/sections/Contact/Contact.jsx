4b184eead15b0dde8cd6118429cd38cf44302666
Cristhian <jobs.cluna@gmail.com>
2022-09-24 14:17:33 -0500

    feat: add Contact comp section

---
+++ src/pages/Home/sections/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact-wrapper'>
+      Contact
+    </div>
+  )
+}
+
+export default Contact
